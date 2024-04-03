5b872cd50ebc039567c6d1eccb3a3bd579e0ee08
remedialchaostheory <remedialchaostheory@users.noreply.github.com>
2024-04-02 23:15:08 -0700

    add basic code outline

--- plugin.js
+++ plugin.js
@@ -1,3 +1,15 @@
-// Given a note (template) will populate current or given
-// note with tasks
+// Given a note (template) will populate current or given note with tasks
+//
+// Note template format:
+// 7 am - wake up, meditate
+// 8 am - workout
+// 6 pm - dinner
+
+// set template note
+//
+
+// parse lines
+
+//
+
 //
