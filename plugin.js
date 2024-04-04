817ba130332e8a5b766b16b9cf5fe29b3dbb8559
remedialchaostheory <remedialchaostheory@users.noreply.github.com>
2024-04-03 23:35:42 -0700

    set template note

--- plugin.js
+++ plugin.js
@@ -5,11 +5,29 @@
 // 8 am - workout
 // 6 pm - dinner
 
-// set template note
 //
 
-// parse lines
+let plugin = {
+  settings: {
+    templateNoteUUID: null,
+  },
 
-// get current note
+  dailyTasks: {
+    async "Add daily tasks to current note"(app, noteHandle) {
+      // set template note
+      const templateNote = await app.findNote({
+        uuid: this.settings.templateNoteUUID,
+      });
+      console.log(JSON.stringify(templateNote));
 
-// tasks to current note
+      let templateContents = await app.getNoteContent(templateNote);
+      console.log(templateContents);
+
+      // parse content/lines
+
+      // get current note
+
+      // tasks to current note
+    },
+  },
+};
